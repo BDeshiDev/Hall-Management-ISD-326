ef3f3e65126189ac75b99460eb69b190cac72fa6
B-DeshiDev <ffalsoy@gmail.com>
2022-02-07 11:56:40 +0600

    Formtest

--- RoomAllotment/views.py
+++ RoomAllotment/views.py
@@ -1,13 +1,30 @@
-from django.http import HttpResponse
+from django.http import HttpResponse, HttpResponseRedirect
 from django.http import Http404
 from django.shortcuts import render
 from .models import *
+from .forms import *
 
 
 def view_requests(request):
     requests = RoomAllotmentRequest.objects.all()
     context = {'requests': requests}
     return render(request, 'polls/View-Requests.html', context)
+
+
+def request_form(request):
+    if request.method == 'POST':
+        # create a form instance and populate it with data from the request:
+        form = RoomAllotmentRequestForm(request.POST)
+        # check whether it's valid:
+        if form.is_valid():
+            print(form.cleaned_data)
+            form.save()
+            return HttpResponseRedirect('/RoomAllotment/requests/')
+        # TODO SHOW ERROR
+    else:
+        form = RoomAllotmentRequestForm()
+        context = {'form': form}
+        return render(request, 'polls/Request-Create-Form.html', context)
 
 
 def request_detail(request, request_id):
--- hallmanagement/settings.py
+++ hallmanagement/settings.py
@@ -37,7 +37,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'RoomAllotment.apps.RoomAllotConfig'
+    'RoomAllotment.apps.RoomAllotConfig',
+    'crispy_forms',
 ]
 
 MIDDLEWARE = [
