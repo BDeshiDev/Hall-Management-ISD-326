34d51c519551b7df6a4f76fa089a9eec28e6d5b4
B-DeshiDev <ffalsoy@gmail.com>
2022-02-18 10:25:39 +0600

    moved to class view

--- RoomAllotment/views.py
+++ RoomAllotment/views.py
@@ -25,7 +25,49 @@ class ProvostHomeView(View):
 
 class LoginView(View):
     def get(self, request, *args, **kwargs):
-        return HttpResponse('login view')
+        if request.method == 'POST':
+            # create a form instance and populate it with data from the request:
+            form = LoginForm(request.POST)
+            # check whether it's valid:
+            if form.is_valid():
+                form.clean()
+                print("login form: " + str(form.cleaned_data))
+
+                user_identifier = form.cleaned_data['userIdentifier']
+                password = form.cleaned_data['password']
+
+                try:
+                    student_id = int(user_identifier)
+                    if login_user_student(request, student_id, password):
+                        print("student login", get_user(request))
+
+                        return HttpResponseRedirect(reverse('student-home'))
+                    else:
+                        print("student login fail but userID is integer", form.cleaned_data)
+                        return HttpResponse("provost login fail")
+                except ValueError:
+                    # assume that this is provost login since userID != int
+                    if login_user_provost(request, user_identifier, password):
+                        print("provost login", get_user(request))
+                        return HttpResponseRedirect(reverse('student-home'))
+                    else:
+                        print("provost login fail", form.cleaned_data)
+                        return HttpResponse("provost login fail")
+            else:
+                print("form fail")
+                print(form.errors)
+                return HttpResponse(form.errors)
+        else:
+            form = LoginForm()
+            context = {'form': form}
+            return render(request, 'RoomAllotment/login.html', context)
+
+
+class logoutView(View):
+    def get(self, request, *args, **kwargs):
+        log_out(request)
+        return HttpResponseRedirect(reverse('login'))
+
 
 class StudentRoomReqView(View):
     def get(self, request, *args, **kwargs):
@@ -65,50 +107,6 @@ def request_detail(request, request_id):
     except RoomAllotmentRequest.DoesNotExist:
         raise Http404("Request does not exist")
     return render(request, 'polls/Request-Detail.html', {'request': request})
-
-
-def login(request):
-    if request.method == 'POST':
-        # create a form instance and populate it with data from the request:
-        form = LoginForm(request.POST)
-        # check whether it's valid:
-        if form.is_valid():
-            form.clean()
-            print("login form: " + str(form.cleaned_data))
-
-            user_identifier = form.cleaned_data['userIdentifier']
-            password = form.cleaned_data['password']
-
-            try:
-                student_id = int(user_identifier)
-                if login_user_student(request, student_id, password):
-                    print("student login", get_user(request))
-
-                    return HttpResponseRedirect(reverse('home'))
-                else:
-                    print("student login fail but userID is integer", form.cleaned_data)
-                    return HttpResponse("provost login fail")
-            except ValueError:
-                # assume that this is provost login since userID != int
-                if login_user_provost(request, user_identifier, password):
-                    print("provost login",  get_user(request))
-                    return HttpResponseRedirect(reverse('home'))
-                else:
-                    print("provost login fail", form.cleaned_data)
-                    return HttpResponse("provost login fail")
-        else:
-            print("form fail")
-            print(form.errors)
-            return HttpResponse(form.errors)
-    else:
-        form = LoginForm()
-        context = {'form': form}
-        return render(request, 'polls/login.html', context)
-
-
-def logout(request):
-    log_out(request)
-    return HttpResponseRedirect('/login')
 
 
 def home(request):
--- hallmanagement/urls.py
+++ hallmanagement/urls.py
@@ -15,7 +15,6 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import include, path
-from RoomAllotment.views import login, logout, home
 
 urlpatterns = [
     #path('RoomAllotment/', include('RoomAllotment.urls')),
