e98e55d623410f3c66d3444c57805abcf282f3e1
upobir <red.abiir@gmail.com>
2022-02-19 18:04:39 +0600

    hooked up basic student views - sabbir

--- RoomAllotment/views.py
+++ RoomAllotment/views.py
@@ -10,15 +10,44 @@ from django.views import View
 
 class HomeView(View):
     def get(self, request, *args, **kwargs):
-        #for key, value in request.session.items():
-        #    print('{} => {}'.format(key, value))
-        return render(request, 'RoomAllotment/home_page.html')
+
+        context = {"loggedIn": is_logged_in(request) }
+        if context["loggedIn"]:
+            user = get_user(request)
+            context["profileUrl"] = f'student/{user.stdID}' if user.is_student() else f'provost/{user.provostID}'
+
+        return render(request, 'RoomAllotment/home_page.html', context)
 
 class StudentHomeView(View):
-    # TODO check if logged in
     def get(self, request, *args, **kwargs):
+        if not is_logged_in(request):
+            return HttpResponseRedirect(reverse('home'))
+        
+        user = get_user(request)
+        if not user.is_student():
+            return HttpResponseRedirect(reverse('home'))
+
         std_id = kwargs['std_id']
-        return render(request, 'RoomAllotment/student_profile.html')
+
+        if user.stdID != std_id:
+            return HttpResponseRedirect(reverse('home'))
+
+
+        context = {
+            "name" : user.name,
+            "cgpa" : user.cgpa,
+            "student_id" : user.stdID,
+            "mobile_no" : user.mobile_no,
+            "level" : user.level,
+            "term" : user.term,
+            "present_address" : user.present_address,
+            "permanent_address" : user.permanent_address,
+            "email" : user.email,
+            "department" : user.department,
+            "roomNo" : user.roomNo,
+        }
+
+        return render(request, 'RoomAllotment/student_profile.html', context)
 
 class ProvostHomeView(View):
     # TODO check if logged in
@@ -27,11 +56,14 @@ class ProvostHomeView(View):
         return render(request, 'RoomAllotment/provost_profile.html')
 
 class LoginView(View):
-    # TODO check if logged in
     def get(self, request, *args, **kwargs):
+        if is_logged_in(request):
+            return HttpResponse("Already logged in")
         return render(request, 'RoomAllotment/login.html')
 
     def post(self, request, *args, **kwargs):
+        if is_logged_in(request):
+            return HttpResponse("Already logged in")
         form = LoginForm(request.POST)
         if form.is_valid():
             form.clean()
@@ -60,17 +92,53 @@ class LoginView(View):
             return HttpResponse(form.errors)
 
 class LogoutView(View):
-    # TODO check if logged in
     def get(self, request, *args, **kwargs):
+        if not is_logged_in(request):
+            return HttpResponseRedirect('home')
+
         log_out(request)
         return HttpResponseRedirect(reverse('login'))
 
 
 class StudentRoomReqView(View):
-    # TODO check if logged in
     def get(self, request, *args, **kwargs):
+        if not is_logged_in(request):
+            return HttpResponseRedirect(reverse('home'))
+        
+        user = get_user(request)
+        if not user.is_student():
+            return HttpResponseRedirect(reverse('home'))
+
         std_id = kwargs['std_id']
+
+        if user.stdID != std_id:
+            return HttpResponseRedirect(reverse('home'))
+        std_id = kwargs['std_id']
+
+        # TODO get free rooms and return them
+
         return render(request, 'RoomAllotment/room.html');
+
+
+    def post(self, request, *args, **kwargs):
+        if not is_logged_in(request):
+            return HttpResponseRedirect(reverse('home'))
+        
+        user = get_user(request)
+        if not user.is_student():
+            return HttpResponseRedirect(reverse('home'))
+
+        std_id = kwargs['std_id']
+
+        if user.stdID != std_id:
+            return HttpResponseRedirect(reverse('home'))
+        std_id = kwargs['std_id']
+
+
+        # TODO create request
+
+        return 
+        
 
 class ProvostRoomAllotView(View):
     # TODO check if logged in
@@ -79,47 +147,32 @@ class ProvostRoomAllotView(View):
         return render(request, 'RoomAllotment/room_provostSide.html')
 
 
-def view_requests(request):
-    requests = RoomAllotmentRequest.objects.all()
-    context = {'requests': requests}
-    return render(request, 'polls/View-Requests.html', context)
-
-
-def request_form(request):
-    if request.method == 'POST':
-        # create a form instance and populate it with data from the request:
-        form = RoomAllotmentRequestForm(request.POST)
-        # check whether it's valid:
-        if form.is_valid():
-            print(form.cleaned_data)
-            form.save()
-            return HttpResponseRedirect('/RoomAllotment/requests/')
-        # TODO SHOW ERROR
-    else:
-        form = RoomAllotmentRequestForm()
-        context = {'form': form}
-        return render(request, 'polls/Request-Create-Form.html', context)
-
-
-def request_detail(request, request_id):
-    try:
-        request = RoomAllotmentRequest.objects.get(pk=request_id)
-    except RoomAllotmentRequest.DoesNotExist:
-        raise Http404("Request does not exist")
-    return render(request, 'polls/Request-Detail.html', {'request': request})
-
-
-def home(request):
-    user = get_user(request)
-
-    if user:
-        context = {}
-        fill_context(request, context)
-        return render(request, 'polls/home.html', context)
-    else:
-        HttpResponseRedirect('/login')
-
-
-
-
+#def view_requests(request):
+#    requests = RoomAllotmentRequest.objects.all()
+#    context = {'requests': requests}
+#    return render(request, 'polls/View-Requests.html', context)
+#
+#
+#def request_form(request):
+#    if request.method == 'POST':
+#        # create a form instance and populate it with data from the request:
+#        form = RoomAllotmentRequestForm(request.POST)
+#        # check whether it's valid:
+#        if form.is_valid():
+#            print(form.cleaned_data)
+#            form.save()
+#            return HttpResponseRedirect('/RoomAllotment/requests/')
+#        # TODO SHOW ERROR
+#    else:
+#        form = RoomAllotmentRequestForm()
+#        context = {'form': form}
+#        return render(request, 'polls/Request-Create-Form.html', context)
+#
+#
+#def request_detail(request, request_id):
+#    try:
+#        request = RoomAllotmentRequest.objects.get(pk=request_id)
+#    except RoomAllotmentRequest.DoesNotExist:
+#        raise Http404("Request does not exist")
+#    return render(request, 'polls/Request-Detail.html', {'request': request})
 
